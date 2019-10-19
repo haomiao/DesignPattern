abf01ecdb7d0229add48a6e0a5f7f77f5e6c9201
colper <635541412@qq.com>
2019-10-19 12:08:02 +0800

    Update base_template_method.h
    fixed the comments for the class of CBaseTemplateMethod

--- src/GOF Four/Behavioral/template_method_/base_template_method.h
+++ src/GOF Four/Behavioral/template_method_/base_template_method.h
@@ -4,10 +4,10 @@
 ///     All rights reserved.  
 ///  
 /// 	@file   base_template_method.h
-/// 	@brief	ģ�巽������. 
+/// 	@brief	模板方法基类. 
 /// 
-///		��Ҫ�ṩ�ⲿʹ�ýӿں����������ʵ��ĳ���㷨������,
-//      ���������ʵ���Լ��㷨�������ʵ��  
+///		主要提供外部使用接口和其具体子类实现某个算法步骤框架,
+//      具体子类可实现自己算法或各步骤实现  
 /// 
 /// 	@version 		  0.0.1   
 /// 	@author           zhouhao
@@ -20,10 +20,10 @@
 #define     _BASE_TEMPLATE_METHOD_H_
 
 
-//  һ������£����Ӳ���������������ʵ��
-//  ԭ������ĳ���������Լ�DoSomething(ģ�巽��)�����ϲ��������ض���ʵ��
-//  ģ�巽�����ڸı䲿���㷨��ʵ��
-//  ������ģʽ��ͨ��ί�иı������㷨
+//  一般情况下，钩子操作将可以被子类重写或者不重写，即基类可有默认实现而子类可重定义；
+//  原语(具体的抽象类操作)需要子类必须重定义实现。而DoSomething(模板方法)基本上不被子类重定义实现。
+//  模板方法用于改变部分算法或实现
+//  而策略模式是通过委托改变整个算法
 class CBaseTemplateMethod
 {
 public:
@@ -32,24 +32,24 @@ public:
 
 public:
 
-    //  ģ�巽��(Ҳ��ָ���ˣ�����ָ�����̡�˳��ִ���㷨��ʵ��)
+    //  模板方法(也即指明了：按照指定流程、顺序执行算法或实现)
     void DoSomething();
 
 public:
 
-    //  ���Ӳ���
+    //  钩子操作
     virtual void DoSomethingOperator1() = 0;
     virtual void DoSomethingOperator2() = 0;
     virtual void DoSomethingOperator3();
 
 protected:
 
-    //  ԭ�����
+    //  原语操作
     void DoSomethingOriginal();
 
 private:
 
-    //  ����ĳ��������
+    //  具体的抽象类操作
     void DoSomethingSelf();
 
 protected:
